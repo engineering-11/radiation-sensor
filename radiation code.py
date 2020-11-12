0605722b78277be3cf9d827df14b3e649098b62b
grantmills8 <71102570+grantmills8@users.noreply.github.com>
2020-11-12 15:05:15 -0800

    Update 2
    fixed syntax

--- radiation code.py
+++ radiation code.py
@@ -9,14 +9,14 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup((11), GPIO.IN)
 GPIO.add_event_detect((11), GPIO.FALLING)
-TimeStamps = []
+timeStamps = []
 duration = 1000
-Start_time = time.time()
-Stop_time = start_time + duration
+start_time = time.time()
+stop_time = start_time + duration
 while time.time() < stop_time:
     if GPIO.event_detected(channel):
         print(time.time())
-TimeStamps.append(time.time()) 
+timeStamps.append(time.time()) 
 
 
 # In[ ]:
